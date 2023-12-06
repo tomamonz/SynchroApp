324af6715b4683a53ba0ef1dd473e291a47393d5
Tom Zal <Tomasz.Zaleski@fdmgroup.com>
2023-12-06 22:27:06 +0100

    Initial commit

---
+++ synChronized/src/main/java/tom/synChro/synChronized/SynChroApplication.java
@@ -0,0 +1,13 @@
+package tom.synChro.synChronized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynChroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynChroApplication.class, args);
+	}
+
+}
---
+++ synChronized/src/test/java/tom/synChro/synChronized/SynChroApplicationTests.java
@@ -0,0 +1,13 @@
+package tom.synChro.synChronized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynChroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
